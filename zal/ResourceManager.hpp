c4c90f0e6bc09ffa20781798c04b087d92644c28
bajnit <krzysiek.kallas@me.com>
2020-11-12 12:46:01 +0000

    v1

--- zal/ResourceManager.hpp
+++ zal/ResourceManager.hpp
@@ -4,10 +4,49 @@
 
 class ResourceManager
 {
-   double get(){
-        return 0;
-    };
+    public:
     
-    ~ResourceManager(){
-    };
+    ResourceManager(){  // Konstruktor domyślny
+        dane = new Resource;
+    }
+    
+    ResourceManager(const ResourceManager& rm){  //Konstruktor kopiujący
+        dane = new Resource{*rm.dane};
+    }
+    
+    ResourceManager(const ResourceManager&& rm){  //Konstruktor przenoszący
+        dane = rm.dane;
+        rm.dane=nullptr;
+    }
+    
+    ResourceManager& operator=(const ResourceManager& rm){  // kopiujacy przypisania
+            
+        if(&rm != this){
+            delete dane;
+            dane=new Resource{*rm.dane};
+        }
+        return *this;
+    }
+    
+    ResourceManager& operator=(const ResourceManager&& rm){  // przenoszący przypisania
+        
+        if(&rm != this){
+            delete dane;
+            dane=rm.dane;
+            rm.dane=nullptr;
+        }
+        return *this;
+    }
+    
+    
+    ~ResourceManager(){  //destruktor
+        delete dane;
+    }
+    
+    double get(){ // funkcja zwracająca get
+        return dane->get();
+     }
+    
+private:
+    Resource *dane; // klasa
 };
